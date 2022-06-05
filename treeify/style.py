611d11505a6017c452da7d58828c0aac51b27856
fwcd <fwcdmail@gmail.com>
2022-06-05 03:09:42 +0200

    Add a bunch of styles

--- treeify/style.py
+++ treeify/style.py
@@ -7,8 +7,15 @@ class Style:
     last_prefix: str
 
 STYLES = {
-    'ascii': Style(indent_prefix='|', t_prefix='+', last_prefix='+'),
-    'ascii2': Style(indent_prefix='|', t_prefix='+', last_prefix='\\'),
+    'ascii': Style(indent_prefix='|', t_prefix='+-', last_prefix='\\-'),
+    'ascii2': Style(indent_prefix='|', t_prefix='+-', last_prefix='`-'),
+    'ascii-compact': Style(indent_prefix='|', t_prefix='+', last_prefix='\\'),
+    'ascii2-compact': Style(indent_prefix='|', t_prefix='+', last_prefix='`'),
+    'arrows': Style(indent_prefix='|', t_prefix='->', last_prefix='->'),
+    'harrows': Style(indent_prefix='|', t_prefix='#>', last_prefix='#>'),
+    'bars': Style(indent_prefix='|', t_prefix='|', last_prefix='|'),
     'yaml': Style(indent_prefix='', t_prefix='-', last_prefix='-'),
+    'empty': Style(indent_prefix='', t_prefix='', last_prefix=''),
+    'compact': Style(indent_prefix='│', t_prefix='├', last_prefix='└'),
     'unicode': Style(indent_prefix='│', t_prefix='├─', last_prefix='└─'),
 }
